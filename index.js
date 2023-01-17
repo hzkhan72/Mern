4c857b022698d61ae4f7c20385df71a180688faf
hzkhan72 <90990714+hzkhan72@users.noreply.github.com>
2023-01-17 10:20:50 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+var cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.use(cors({ origin: "http://localhost:3000" }));
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+app.use("/api/insurances", require("./routes/api/insurance"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
